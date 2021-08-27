6c2a748b192f46c6a763dd88014275ce829f8643
William McCarty <kcchief@yahoo.com>
2021-08-26 23:23:01 -0500

    Add more images and captain page

---
+++ Heidi/js/main.js
@@ -0,0 +1,17 @@
+﻿jQuery(document).ready(function () {
+    console.log("jQuery is working!");
+});
+
+$(function () {
+    $(".thumb").on("click", function () {
+        var imgSrc = $(this).attr("src");
+        $("#lightbox-content").html('<img src="' + imgSrc + '" class="img-responsive-lightBox maxsize" />');
+        $("#lightbox-container").fadeIn(1300);
+        $("#lightbox-container").on("click", function () {
+            $(this).fadeOut(500);
+        });
+
+    });
+
+
+});
